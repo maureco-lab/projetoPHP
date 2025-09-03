8fb70766f69195ec5f95d63d9d8018c00f21bd77
maureco-lab <mauriciomrm04@gmail.com>
2025-09-02 21:15:26 -0300

    impostao de rendao

--- imposto.php
+++ imposto.php
@@ -2,9 +2,9 @@
 
 
 
-$vb= 4708;
-$por= 0.12;
-$desc= $vb*$por; 
+$vb= 4300;
+$por= 14;
+$desc= $vb*$por/100; 
 $descir=0;
 $sl= 0;
 
@@ -14,26 +14,26 @@ echo ("Desconto do INSS: R$$desc <br/>");
 
 
 if ($vb>0 && $vb<3036) {
-	$ir=0;
+	$ir= 0;
 
 } elseif ($vb>3036 && $vb<3533) {
-	$ir=0.07;
+	$ir= 7;
 
 } elseif ($vb>3533 && $vb<4688) {
-	$ir= 0.15;
+	$ir= 15;
 
 } elseif ($vb>4688 && $vb<5830) {
-	$ir= 0.22;
+	$ir= 22.5;
 
 } elseif ($vb>5830) {
-	$ir= 0.27;
+	$ir= 27.5;
 }
 
 function imposto($vb, $ir, $descir, $desc ,  $sl){
-	$descir= $vb*$ir;
+	$descir= $vb*$ir/100;
 	$sl= $vb-$desc-$descir;
 	echo("Desconto IR: R$$descir <br/>");
-	echo ("Salario Liquido: R$$sl"); 
+	echo ("Salario Liquido: R$$sl");
 }
 
 	imposto($vb, $ir, $descir, $desc , $sl);
