baa3086c6b7753c0bab9d3ea852c35999a5331a2
maureco-lab <mauriciomrm04@gmail.com>
2025-09-09 20:12:17 -0300

    exercicios

---
+++ while.php
@@ -0,0 +1,36 @@
+<?php 
+
+/*$num1= 10;
+$num2= 15;
+$soma= $num1+$num2;
+
+if ($soma>20) {
+	$soma= $soma + 8;
+	echo ("O número é maior que 20, entao foi somado mais 8<br/>");
+	echo ($soma);
+
+} else{
+	$soma= $soma - 5;
+	echo ("O número é menor que 20, entao foi subtraido 5 do valor<br/>");
+	echo ($soma);
+}
+
+*/
+
+$num1= 3;
+
+if ($num1 % 10==0 ||$num1 % 5==0 || $num1 % 2==0  ) {
+	echo ($num1);
+	echo (" É divisivel por 10, por 5 ou por 2 <br/>");
+	// code...
+} else {
+	echo ($num1);
+	echo (" Não é divisivel por 10, por 5 ou por 2 <br/>");
+}
+
+
+
+
+
+
+ ?>
