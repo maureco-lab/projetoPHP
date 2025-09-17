a829362ded8c9594051b86512d47b1fc96fccff8
maureco-lab <mauriciomrm04@gmail.com>
2025-09-16 21:18:52 -0300

    while2

--- while2.php
+++ while2.php
@@ -1,24 +1,48 @@
 <?php 
-
-$num1= 2;
+/*
+$num1= 0;
 $num2=8;
 $cont=0;
 
 
-while ($cont>5 || $num1!=0 || $num2!=0) {
-	
+while ($cont>3 || $num1!=0 || $num2!=0) {
+
 	$cont++;
 	if ($num1>$num2) {
 	 echo ("O maior número é o $num1<br/>");
 
-} elseif($num2>$num1){
-	echo ("O maior número é o $num2<br/>");
-
 } else{
+	echo ("O maior número é o $num2<br/>");
+} 
+	
 	echo ("A entrada é 0");
 }
+	
 
+
+$cont=1;
+
+while ($cont<100) {
+	$cont++;
+	echo ("$cont<br/>");
+
+	if ($cont % 10 ==0) {
+		echo ("$cont é múltiplo de 10<br/>");
+		
+	}
+	
 }
 
+*/
 
+$cont=0;
+$num1=1;
+
+while ($cont<64) {
+	$cont++;
+	
+	echo ("$num1<br/>");
+	$num1= $num1*2;	
+	// code...
+}
  ?> 
