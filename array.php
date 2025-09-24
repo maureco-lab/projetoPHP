83f3e4f72043a4e55c6cf6d0f28fa347f2347020
maureco-lab <mauriciomrm04@gmail.com>
2025-09-23 21:18:36 -0300

    array

--- array.php
+++ array.php
@@ -50,6 +50,15 @@ foreach ($num as $valor) {
 	echo("Valor é $valor <br/>");
 }
 
+$num[0]= "um";
+$num[1]= "dois";
+$num[2]= "três";
+$num[3]= "quatro";
+$num[4]= "cinco";
 
+foreach ($num as $valor) {
+	echo("Valor é $valor <br/>");
+	// code...
+}
 
  ?>
