ae3957d7c3ff1e552e2905b938b79ec24743df99
maureco-lab <mauriciomrm04@gmail.com>
2025-09-30 21:43:27 -0300

    matriz

--- matrizex.php
+++ matrizex.php
@@ -27,7 +27,8 @@ for ($l=0; $l <3 ; $l++) {
 }
 echo("<pre/>");
 print_r($mr);
-*/
+================================================
+
 
 
 $ma= array(
@@ -40,6 +41,20 @@ $mb= array(
 	array(98,65, 35),
 	array(5, 27, 8),
 	array(74, 14, 3));
+
+$mr= array(
+	array(),
+	array(),
+	array(),);
+
+for ($l=0; $l <3 ; $l++) { 
+	for ($c=0; $c < 3; $c++) { 
+		$mr[$l][$c]= $ma[$l][$c] + $mb[$l][$c];
+		// code...
+	}
+	// code...
+
+}
 
 $mra=0;
 $mrb=0;
@@ -62,5 +77,37 @@ print_r("Matriz 2: $mrb");
 
 echo("<pre/>");
 print_r("Matriz R: $mrr");
+
+====================================
+*/
+
+$al= array(
+	array("n" =>"João", "a" => 10),
+	array("n" =>"Pedro","a" => 7),
+	array("n" =>"Jorge","a" => 8),
+	array("n" =>"Cléebert","a" => 7),
+	array("n" =>"Marcia","a" => 10),
+	array("n" =>"Lúcia","a" => 5),
+	array("n" =>"Alessandra","a" => 10),
+	array("n" =>"Lulu","a" =>5),
+	array("n" =>"Manu","a" =>2),
+	array("n" =>"Theo","a" => 5));
+
+$soma=0;
+$maiorn=0;
+$alunomaior="";
+
+foreach ($al as $aluno) {
+	$soma= $soma + $aluno['a'];
+	if ($aluno['a'] > $maiorn) {
+		$maiorn= $aluno['a'];
+		$alunomaior= $aluno['n'];
+		// code...
+	}
+	// code...
+}
+$media = $soma /10;	
+echo ("O aluno com maior notá é: $alunomaior <br/>");
+echo ("A média de nota da turma é : $media");
 
  ?>
