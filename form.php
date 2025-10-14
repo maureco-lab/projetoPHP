87111e084a09b987b661b2c2d3026008be424c5f
maureco-lab <mauriciomrm04@gmail.com>
2025-10-14 19:43:37 -0300

    form

---
+++ form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Form</title>
+</head>
+<body>
+
+	<h1>Formulários</h1>
+
+	<form action="cadastrar.php" method="post">
+
+		<label for="nome">Nome:</label>
+		<br>
+		<input type="text" name="nome" id="nome">
+		<br><br>
+		<label for="email">Email:</label>
+		<br>
+		<input type="email" name="email" id="email">
+		<br><br>
+		<input type="submit" name="enviar">
+		<button type="submit">Cadastrar</button>
+	</form>
+
+	
+
+</body>
+</html>
