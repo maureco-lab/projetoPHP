169fe8cc427a65a514f721eae393e11163f7c882
maureco-lab <mauriciomrm04@gmail.com>
2025-10-14 19:54:21 -0300

    form

--- cadastrar.php
+++ cadastrar.php
@@ -1,5 +1,15 @@
 <?php 
 
+ $nome = $_POST['nome'];
+ $email= $_POST['email'];
+
+ if ($nome=="MAURICIO" && $email=="mauricio@etec.com") {
+ 	echo "Bem vindo $nome!!";
+ 	
+ } else {
+ 	echo "Usuario Invalido!";
+ }
  
+
 
  ?>
