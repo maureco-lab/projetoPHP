7c025abaa439941e3bbacd7aae35a039e1a067de
maureco-lab <mauriciomrm04@gmail.com>
2025-10-14 20:16:12 -0300

    form

--- cadastrar.php
+++ cadastrar.php
@@ -3,8 +3,14 @@
  $nome = $_POST['nome'];
  $email= $_POST['email'];
 
- if ($nome=="MAURICIO" && $email=="mauricio@etec.com") {
- 	echo "Bem vindo $nome!!";
+ if ($nome==="MAURICIO" && $email==="mauricio@etec.com") {
+ 	echo "<table border=1>
+ 		<tr>
+ 		<td>Bem vindo</td> 
+ 		</tr>
+ 		<td>$nome!! </td>
+ 		</tr>
+ 	</table>";
  	
  } else {
  	echo "Usuario Invalido!";
