976f73d48a7ebc8495a4a50d15f1118228bce0ad
maureco-lab <mauriciomrm04@gmail.com>
2025-10-21 19:46:38 -0300

    index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
 	<meta charset="utf-8">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
@@ -11,6 +11,26 @@
 
 	<h1>Cadastro de Usuários</h1>
 
+	<form action="#" method="post">
+		<label for="primeiroNome">Primeiro Nome</label>
+		<input type="text" name="primeiroNome" id="primeiroNome" placeholder="Nome" maxlength="50" required autofocus>
+		<label for="segundoNome">Segundo Nome</label>
+		<input type="text" name="segundoNome" id="segundoNome" placeholder="Sobrenome" maxlength="50" required>
+		<label for="escolhaPets" id="escolhaPets" name="escolhaPets">Escolha seu Pet</label>
+		<select>
+			<option value="nenhum" >Nenhum</option>
+			<option value="cachorro" >Cachorro</option>
+			<option value="gato" >Gato</option>
+			<option value="peixe" >Peixe</option>
+			<option value="passaro" >Pássaro</option>		
+			<option value="boi" >Boi</option>		
+			<option value="pato" >Pato</option>		
+		</select>
+		<button type="submit">Enviar</button>
+		<button type="reset">Limpar</button>
+		
+
+	</form>
 
 
 
@@ -18,6 +38,10 @@
 
 
 
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 </body>
 </html>
