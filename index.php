80fb25af6fab231c738ac7512ad06a5c41ecaff0
maureco-lab <mauriciomrm04@gmail.com>
2025-10-21 19:31:36 -0300

    index
    ;

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<title>Formulário de Cadastro</title>
+</head>
+<body>
+
+	<h1>Cadastro de Usuários</h1>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
