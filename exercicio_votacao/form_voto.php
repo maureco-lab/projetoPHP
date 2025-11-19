523a1b31f4f723a3b6834c35ac10ccc9407a7aae
maureco-lab <mauriciomrm04@gmail.com>
2025-11-18 22:07:36 -0300

    votos

--- exercicio_votacao/form_voto.php
+++ exercicio_votacao/form_voto.php
@@ -3,12 +3,14 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Formulário - Maior Time de SP</title>
 </head>
 <body>
 
 	<form action="exercicio_voto.php" method="get">
 		<h2>Qual o maior time de São Paulo? Sem clubismo (Sabendo que é o São Paulo FC)</h2>
+
 
 		<input type="radio" id="sao_paulo" name="voto" value="Sao Paulo">
 		<label for="sao_paulo">São Paulo</label>
